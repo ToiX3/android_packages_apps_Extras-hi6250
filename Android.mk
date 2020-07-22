bc2986e7c7ae907d0aa4f02e3afa2f88473f3061
GG2501YT <gameglitscher@linux4.de>
2020-07-22 15:23:07 +0200

    updated Extras

--- Android.mk
+++ Android.mk
@@ -2,10 +2,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SDK_VERSION := current
-LOCAL_PACKAGE_NAME := HuaweiSettings
+LOCAL_PACKAGE_NAME := Extras
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-# Huawei Settings for hi6250 LineageOS (by dady8889)
-
-<img width="200" alt="1" src="https://i.imgur.com/eG4HfYG.png">    <img width="200" alt="2" src="https://i.imgur.com/E6XigpO.png">
+# Extras for hi6250 LineageOS (by dady8889)
 
